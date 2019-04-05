dd9ac5c0b1c76b161ea84de526be39da154b3ab4
calgacus <cory_taylor@yahoo.com>
2019-04-04 23:58:33 -0400

    moved dict files back out of the jar

--- src/org/jl/nwn/spell/Dictionaries.java
+++ src/org/jl/nwn/spell/Dictionaries.java
@@ -26,18 +26,10 @@ public class Dictionaries {
     static{
         try{
             InputStream is = Dictionaries.class.getClassLoader().getResourceAsStream(
-                    "/dict/dictionaries.properties");
+                    "dict/dictionaries.properties");
             if ( is != null ) {
                 props.load(is);
-                System.out.println("found props 1 " );
-            }else {
-                InputStream is2 = Dictionaries.class.getClassLoader().getResourceAsStream(
-                        "resource/dict/dictionaries.properties");
-                if ( is2 != null ) {
-                    props.load(is2);
-                    System.out.println("found props 2 " );
-                }
-
+                System.out.println("found props  " );
             }
         } catch (IOException ioex){
             ioex.printStackTrace();
@@ -46,38 +38,29 @@ public class Dictionaries {
     
     public static SpellDictionary forLanguage( NwnLanguage lang ){
         String dictName = (String) props.get(lang.getLocale().getLanguage());
+        OpenOfficeSpellDictionary dict = null;
         if ( dictName != null ){
-            if ( dicts.containsKey(lang) )
+            if ( dicts.containsKey(lang) ) {
                 return dicts.get(lang);
+            }
             System.out.println("loading dictionary for : " + lang);
             URL url = Dictionaries.class.getClassLoader().getResource(dictName);
             System.out.println(url);
             try {
-                long time = System.currentTimeMillis();
                 System.out.println("loading dictionary for dictname : " + dictName);
                 System.out.println("loading dictionary for url : " + url.toString());
                 File zip = new File(url.toURI());
-                //File userDict = new File(zip.getParentFile(), lang.getName()+".userDict");
-                //userDict.createNewFile();
-                OpenOfficeSpellDictionary dict = new OpenOfficeSpellDictionary(
-                        new ZipFile(zip)//, false
-                        //url.openStream(),
-                        //userDict
-                        );
-                dicts.put( lang, dict );
-                System.out.println(System.currentTimeMillis()-time);
-                return dict;
+                dict = new OpenOfficeSpellDictionary( new ZipFile(zip) );
             } catch (Exception e){
                 e.printStackTrace();
-                dicts.put( lang, null );
             }
         }else{
             System.out.println("dictname is null " );
             System.out.println("lang is  " + lang.getLocale().getLanguage());
             System.out.println("props " + props.toString() );
-
         }
-        return null;
+        dicts.put( lang, dict );
+        return dict;
     }
     
 }
