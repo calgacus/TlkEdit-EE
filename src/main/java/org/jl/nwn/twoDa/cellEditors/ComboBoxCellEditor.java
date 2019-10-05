9a313a5af6a5046f4a381378d163e7f7c879487f
Mingun <Alexander_Sergey@mail.ru>
2019-10-06 00:26:48 +0500

    Cleanup: Map ESC to cancel action in all constructors of combobox cell editor for 2da files

--- src/main/java/org/jl/nwn/twoDa/cellEditors/ComboBoxCellEditor.java
+++ src/main/java/org/jl/nwn/twoDa/cellEditors/ComboBoxCellEditor.java
@@ -18,42 +18,40 @@ import org.w3c.dom.NodeList;
 
 public class ComboBoxCellEditor extends DefaultCellEditor {
 
-	JComboBox comboBox;
+    public ComboBoxCellEditor() {
+        this(new JComboBox<>());
+    }
 
-	public ComboBoxCellEditor(){
-		super( new JComboBox() );
-		comboBox = (JComboBox) super.editorComponent;
-		comboBox.getInputMap( JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT ).put( KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0 ), "cancel" );
-		comboBox.getActionMap().put( "cancel", new AbstractAction(){ @Override public void actionPerformed( ActionEvent e ){ cancelCellEditing(); } } );
-	}
+    public ComboBoxCellEditor(JComboBox<String> comboBox) {
+        super(comboBox);
+        comboBox.getInputMap( JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT ).put(
+            KeyStroke.getKeyStroke( KeyEvent.VK_ESCAPE, 0 ), "cancel"
+        );
+        comboBox.getActionMap().put("cancel", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) { cancelCellEditing(); }
+        });
+    }
 
-	public ComboBoxCellEditor( JComboBox b ){
-		super( b );
-	}
-
-	public ComboBoxCellEditor( Element e ){
-		this();
-		boolean editable = false;
-		Node n = e.getAttributes().getNamedItem( "editable" );
-		if ( n!=null )
-			editable = Boolean.valueOf( n.getNodeValue() ).booleanValue();
-		NodeList entries = e.getElementsByTagName( "entry" );
-		String[] values = new String[ entries.getLength() ];
-		for ( int entryNum = 0; entryNum < entries.getLength(); entryNum++ ){
-			Element entryNode = (Element) entries.item( entryNum );
-			values[ entryNum ] = entryNode.getAttributes().getNamedItem( "value" ).getNodeValue();
-		}
-		comboBox.setModel( new DefaultComboBoxModel( values ) );
-		comboBox.setEditable( editable );
-	}
-
-	public static void main(String[] args) {
-	}
+    public ComboBoxCellEditor(Element e) {
+        this();
+        final Node n = e.getAttributes().getNamedItem( "editable" );
+        final boolean editable = n != null && Boolean.parseBoolean(n.getNodeValue());
+        final NodeList entries = e.getElementsByTagName( "entry" );
+        final String[] values = new String[ entries.getLength() ];
+        for (int i = 0; i < values.length; ++i) {
+            final Element entryNode = (Element) entries.item( i );
+            values[i] = entryNode.getAttributes().getNamedItem( "value" ).getNodeValue();
+        }
+        final JComboBox<String> comboBox = (JComboBox<String>) super.editorComponent;
+        comboBox.setModel( new DefaultComboBoxModel<>( values ) );
+        comboBox.setEditable( editable );
+    }
 
     @Override
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-        Component retValue;
-        retValue = super.getTableCellEditorComponent(table, value, isSelected, row, column);
+        final Component retValue = super.getTableCellEditorComponent(table, value, isSelected, row, column);
+        final JComboBox<String> comboBox = (JComboBox<String>) super.editorComponent;
         comboBox.getEditor().selectAll();
         return retValue;
     }
