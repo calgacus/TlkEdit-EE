2b16e1e3c3805ba9a52eb21b0874867f4bfd729c
Mingun <Alexander_Sergey@mail.ru>
2019-10-21 00:54:53 +0500

    ERF: Refactor ERF editor, make it more consistent, fix some minor bugs
    Fix bug: when new ERF is created, it reports its type as null, regardless of that was supplied in the constructor

--- src/main/java/org/jl/nwn/erf/ErfEdit.java
+++ src/main/java/org/jl/nwn/erf/ErfEdit.java
@@ -46,12 +46,8 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellEditor;
 
 import org.jdesktop.swingx.JXTable;
-import org.jl.nwn.NwnLanguage;
 import org.jl.nwn.Version;
 import org.jl.nwn.editor.SimpleFileEditorPanel;
-import org.jl.nwn.gff.CExoLocSubString;
-import org.jl.nwn.gff.Gff;
-import org.jl.nwn.gff.GffCExoLocString;
 import org.jl.nwn.gff.editor.CExoLocStringEditor;
 import org.jl.nwn.resource.ResRefUtil;
 import org.jl.nwn.resource.ResourceID;
@@ -80,6 +76,7 @@ public class ErfEdit extends SimpleFileEditorPanel{
 
     private final JComboBox<ErfFile.ErfType> cbTypeSelector = new JComboBox<>(ErfFile.TYPES);
     private final CExoLocStringEditor descEditor = new CExoLocStringEditor();
+    /** GUI element with ERF description data. */
     private final Box descriptionBox = new Box(BoxLayout.Y_AXIS);
 
     private final JFileChooser fChooser = new JFileChooser( new File(".") );
@@ -100,22 +97,11 @@ public class ErfEdit extends SimpleFileEditorPanel{
 
     private JXTable table = new JXTable(contentModel);
 
-    private final TransferHandler fileDropHandler = new FileDropHandler() {
-        @Override
-        public void importFiles(List<File> files) {
-            for ( File f : files )
-                erf.putResource(f);
-            contentModel.reload();
-        }
-    };
     //<editor-fold defaultstate="collapsed" desc="Actions">
     private final Action actNew = new AbstractAction("New ERF") {
         @Override
         public void actionPerformed(ActionEvent e) {
-            GffCExoLocString desc = new GffCExoLocString( "erf_desc" );
-            erf = new ErfFile( new File("new erf"), ErfFile.ERF, desc  );
-            contentModel.reload();
-            actSave.setEnabled(false);
+            setErf(new ErfFile(ErfFile.ERF));
         }
     };
 
@@ -127,7 +113,7 @@ public class ErfEdit extends SimpleFileEditorPanel{
             fChooser.setFileFilter( fFilterErf );
             if ( fChooser.showOpenDialog( table ) == JFileChooser.APPROVE_OPTION )
                 try{
-                    open( fChooser.getSelectedFile() );
+                    setErf(new ErfFile(fChooser.getSelectedFile()));
                 } catch ( IOException ioex ){
                     JOptionPane.showMessageDialog( table, ioex, "error : open failed", JOptionPane.ERROR_MESSAGE );
                 }
@@ -150,13 +136,14 @@ public class ErfEdit extends SimpleFileEditorPanel{
         public void actionPerformed(ActionEvent e) {
             fChooser.setMultiSelectionEnabled(false);
             fChooser.setFileSelectionMode( JFileChooser.FILES_ONLY );
-            if ( fChooser.showOpenDialog( table ) == JFileChooser.APPROVE_OPTION )
+            if (fChooser.showOpenDialog( table ) == JFileChooser.APPROVE_OPTION) {
                 try{
-                    saveAs( fChooser.getSelectedFile(), erf.getVersion() );
-                    actSave.setEnabled( true );
+                    erf.write(fChooser.getSelectedFile());
+                    setIsModified(false);
                 } catch ( IOException ioex ){
                     JOptionPane.showMessageDialog( table, ioex, "error : save failed", JOptionPane.ERROR_MESSAGE );
                 }
+            }
         }
     };
 
@@ -340,7 +327,6 @@ public class ErfEdit extends SimpleFileEditorPanel{
                     erf.setType( type );
                     descriptionBox.setVisible( type != ErfFile.ERF );
                     sPane.setDividerLocation(sPane.getDividerLocation());
-                    //sPane.setEnabled( type != ErfFile.ERF );
                 }
             }
         });
@@ -369,23 +355,11 @@ public class ErfEdit extends SimpleFileEditorPanel{
         } );
     }
 
-    public ErfEdit( Version nwnVersion ){
+    private ErfEdit(ErfFile erf) {
         super();
-        GffCExoLocString desc = new GffCExoLocString( "erf_desc" );
-        descEditor.setCExoLocString( desc );
-        CExoLocSubString s = new CExoLocSubString(
-                "<HAK NAME>\n<URL>\n<DESCRIPTION>",
-                NwnLanguage.ENGLISH,
-                Gff.GENDER_MALE
-                );
-        desc.addSubstring(s);
-        erf = new ErfFile( new File("new hak"), ErfFile.HAK, desc, nwnVersion );
         setLayout( new BorderLayout() );
-        //add( new JScrollPane(list), BorderLayout.CENTER );
         sPane = new JSplitPane( JSplitPane.VERTICAL_SPLIT );
         sPane.setOneTouchExpandable(true);
-        //add( new JScrollPane(table), BorderLayout.CENTER );
-        //add( descEditor, BorderLayout.SOUTH );
         sPane.add( new JScrollPane( table ) );
         JLabel descLabel = new JLabel();
         I18nUtil.setText(descLabel, "MOD / HAK Description" );
@@ -395,11 +369,9 @@ public class ErfEdit extends SimpleFileEditorPanel{
         sPane.add( descriptionBox );
         sPane.setResizeWeight( 0.8 );
         add( sPane, BorderLayout.CENTER );
-        descEditor.labelBox.setVisible(false);
-        cbTypeSelector.setSelectedItem( erf.getType() );
-        actSave.setEnabled(false);
 
-        //toolbar.add( actSave );
+        descEditor.labelBox.setVisible(false);
+
         Actions.configureActionUI(actAddFiles,uid,"ErfEdit.add");
         Actions.configureActionUI(actRemove,uid,"ErfEdit.remove");
         Actions.configureActionUI(actExtractSelected,uid,"ErfEdit.extract");
@@ -439,29 +411,45 @@ public class ErfEdit extends SimpleFileEditorPanel{
         InputMap im = table.getInputMap(WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
         ActionMap am = table.getActionMap();
         for ( Action a : editActions ){
-            im.put((KeyStroke)a.getValue( a.ACCELERATOR_KEY ), a.getValue( a.ACTION_COMMAND_KEY ));
-            am.put(a.getValue( a.ACTION_COMMAND_KEY ), a );
+            final Object action = a.getValue(Action.ACTION_COMMAND_KEY);
+            im.put((KeyStroke)a.getValue( Action.ACCELERATOR_KEY ), action);
+            am.put(action, a);
         }
 
-
-        //Box b = new Box( BoxLayout.Y_AXIS );
-        //b.add(mbar);
-        //b.add(toolbar);
-
-        //add( toolbar, BorderLayout.NORTH );
-        //add( b, BorderLayout.NORTH );
-        setFileVersion( erf.getVersion() );
-        setTransferHandler(fileDropHandler);
+        setTransferHandler(new FileDropHandler() {
+            @Override
+            public void importFiles(List<File> files) {
+                for (final File f : files) {
+                    erf.putResource(f);
+                }
+                contentModel.reload();
+            }
+        });
         showToolbar(true);
         setVisible( true );
-        setIsModified(false);
+
+        setErf(erf);
     }
 
-    public ErfEdit( File erfFile ) throws IOException{
-        this( Version.getDefaultVersion() );
-        open( erfFile );
-        setIsModified(false);
-        setFileVersion( erf.getVersion() );
+    /**
+     * Create editor with new empty HAK file for specified version of game
+     * (i.e. file format).
+     *
+     * @param nwnVersion Format version of the new file
+     */
+    public ErfEdit(Version nwnVersion) {
+        this(new ErfFile(ErfFile.HAK, nwnVersion));
+    }
+
+    /**
+     * Create editor and open existent file.
+     *
+     * @param erfFile File that must be opened
+     *
+     * @throws IOException If file cann't be readed
+     */
+    public ErfEdit(File erfFile) throws IOException {
+        this(new ErfFile(erfFile));
     }
 
     //<editor-fold defaultstate="collapsed" desc="SimpleFileEditor">
@@ -479,7 +467,6 @@ public class ErfEdit extends SimpleFileEditorPanel{
     @Override
     public void saveAs(File f, Version nwnVersion) throws IOException {
         erf.write(f);
-        actSave.setEnabled(true);
         setIsModified(false);
     }
 
@@ -501,6 +488,12 @@ public class ErfEdit extends SimpleFileEditorPanel{
 
     //<editor-fold defaultstate="collapsed" desc="SimpleFileEditorPanel">
     @Override
+    protected void setIsModified(boolean modified) {
+        super.setIsModified(modified);
+        actSave.setEnabled(modified);
+    }
+
+    @Override
     public JMenu[] getMenus() { return new JMenu[]{menuErf}; }
 
     @Override
@@ -515,21 +508,8 @@ public class ErfEdit extends SimpleFileEditorPanel{
     }
     //</editor-fold>
 
-    public void open(File f) throws IOException {
-        erf = new ErfFile(f);
-        cbTypeSelector.setSelectedItem( erf.getType() );
-        //cbTypeSelector.setEnabled(false);
-        actSave.setEnabled(true);
-        descEditor.setCExoLocString( erf.getDescription() );
-        contentModel.reload();
-    }
-
     public File extractAsTempFile(ResourceID id, boolean replaceWithFile) throws IOException {
         return erf.extractAsTempFile(id, replaceWithFile);
-    }
-
-    public void putResource(ResourceID id, File file) {
-        erf.putResource(id, file);
     }
 
     public ResourceID[] getSelectedResources(){
@@ -540,14 +520,22 @@ public class ErfEdit extends SimpleFileEditorPanel{
         return ids;
     }
 
-    // setup version dependant stuff
-    private void setFileVersion(Version v) {
-        TableCellEditor resRefEd =
+    private void setErf(ErfFile erf) {
+        this.erf = erf;
+        contentModel.reload();
+
+        descEditor.setCExoLocString(erf.getDescription());
+        cbTypeSelector.setSelectedItem(erf.getType());
+
+        // Setup restrictions for resource name length
+        final TableCellEditor resRefEd =
                 new FormattedCellEditor(
                 new JFormattedTextField(
                 ResRefUtil.instance(erf.getVersion())
                 .getStringFormatter(false)));
         table.getColumnModel().getColumn(0).setCellEditor( resRefEd );
+
+        setIsModified(false);
     }
 
     public static boolean accept(File f) {
